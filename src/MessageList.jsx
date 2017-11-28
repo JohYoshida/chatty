518d26e7766ae90ac73f2f6dd8df0357f3f67314
Joh Yoshida <johyoshida@gmail.com>
2017-11-28 12:26:19 -0800

    Component lifecycle methods

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,10 +3,16 @@ import Message from './Message.jsx';
 
 class MessageList extends Component {
   render() {
+    const messages = this.props.messages.map((message) => {
+      return <Message
+        key = { message.id }
+        username = { message.username }
+        content = { message.content } />
+    });
     console.log("Rendering <MessageList/>");
     return (
       <main className="messages">
-        <Message />
+        {messages}
         <div className="message system">
           Anonymous1 changed their name to nomnom.
         </div>
